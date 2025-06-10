4c60a963509063f903dd4a696a4231a36efbfc05
ingyu.suh <sig45@naver.com>
2025-06-10 16:20:39 +0900

    [test] ReadCompare 테스트 추가

--- TestConsole/src/main/java/ReadCompare.java
+++ TestConsole/src/main/java/ReadCompare.java
@@ -1,5 +1,5 @@
 public class ReadCompare{
-    public void execute(String commandStr) {
+    public boolean execute(String commandStr) {
         throw new RuntimeException("아직 구현 안됐어요");
     }
 }
--- TestConsole/src/test/java/ReadCompareTest.java
+++ TestConsole/src/test/java/ReadCompareTest.java
@@ -3,20 +3,39 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class ReadCompareTest {
     @Mock
-    Read read;
+    Write write;
 
     @Mock
-    Write write;
+    ReadCompare readCompare;
 
 
     @Test
-    void readCompareTest
-    doReturn("0x12345678").when(read).getReturn();
+    void ReadCompare_PASS_테스트() {
+        String commandStr = "write 3 0x12345678";
+        doReturn(true).when(readCompare).execute(commandStr);
+
+        write.execute(commandStr);
+        Boolean result = readCompare.execute(commandStr);
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void ReadCompare_FAIL_테스트() {
+        String commandStr = "write 3 0x12345678";
+        doReturn(false).when(readCompare).execute(commandStr);
+
+        write.execute(commandStr);
+        Boolean result = readCompare.execute(commandStr);
+        assertThat(result).isEqualTo(false);
+    }
+
+
 
 
 }
