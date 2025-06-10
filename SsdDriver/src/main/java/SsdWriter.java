90b7d68880b7f6085e1cf24841a01cd9d4627918
dudalss-moon <test@test.com>
2025-06-10 16:22:17 +0900

    [feature] 유효한 주소값 확인

--- SsdDriver/src/main/java/SsdWriter.java
+++ SsdDriver/src/main/java/SsdWriter.java
@@ -1,2 +1,16 @@
 public class SsdWriter {
+    public String write(String number, String s) {
+        int addr = -1;
+        try {
+            addr = Integer.valueOf(number);
+        } catch (NumberFormatException e) {
+            return "ERROR";
+        }
+
+        if (addr < 0 || addr > 99) {
+            return "ERROR";
+        }
+
+        return s;
+    }
 }
