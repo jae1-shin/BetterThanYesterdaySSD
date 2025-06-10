41e0096f66a9ceaa2e79770e5701e147b364b5d8
dudalss-moon <test@test.com>
2025-06-10 16:39:27 +0900

    [feature] 데이터 0x로 시작하지 않는 경우 조건 개발

--- SsdDriver/src/main/java/SsdWriter.java
+++ SsdDriver/src/main/java/SsdWriter.java
@@ -14,6 +14,22 @@ public class SsdWriter {
         if (s.length() != 10) {
             return "ERROR";
         }
+
+        if (!s.startsWith("0x")) {
+            return "ERROR";
+        }
+
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return "ERROR";
+            }
+            if (s.charAt(i) < 'A' || s.charAt(i) > 'Z') {
+                return "ERROR";
+            }
+            if (s.charAt(i) < 'a' || s.charAt(i) > 'z') {
+                return "ERROR";
+            }
+        }
         return s;
     }
 }
