6efeb9978d227b7bcd9d9b08968ceb4c0e8fa39b
dudalss-moon <test@test.com>
2025-06-10 16:21:27 +0900

    [test] 유효한 주소값 확인 테케 추가

---
+++ SsdDriver/src/test/java/SsdWriterTest.java
@@ -0,0 +1,32 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readString;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SsdWriterTest {
+
+    @Test
+    void 두번째_파라미터_0_99_아닌경우_실패() {
+        //arrange
+        SsdWriter ssdWriter = new SsdWriter();
+
+        //act
+        String actual = ssdWriter.write("-1", "0xFFFFFFFF");
+
+        //assert
+        assertThat(actual).isEqualTo("ERROR");
+    }
+
+    @Test
+    void 세번째_파라미터_10글자가_아닌경우_실패() {
+
+    }
+
+}
