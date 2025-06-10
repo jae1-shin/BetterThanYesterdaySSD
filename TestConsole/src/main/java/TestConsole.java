cc4d71f97ded2a646d3f3f480874ce25ca01df6d
magicers <magicers123@gmail.com>
2025-06-10 15:32:19 +0900

    shell Skeleton Code

--- TestConsole/src/main/java/TestConsole.java
+++ TestConsole/src/main/java/TestConsole.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class TestConsole {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Shell> ");
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) continue;
+
+//            String[] parts = line.split("\\s+");
+//            String commandName = parts[0];
+//            String[] commandArgs = new String[parts.length - 1];
+//            System.arraycopy(parts, 1, commandArgs, 0, commandArgs.length);
+
+            if (line.equals("exit")) {
+                System.out.println("종료되었습니다");
+                return;
+            } else {
+                System.out.println("Command 실행");;
+            }
+        }
+    }
 }
