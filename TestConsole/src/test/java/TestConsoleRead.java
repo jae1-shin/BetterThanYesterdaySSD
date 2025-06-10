a2ca7d66e2b6fa20b18d1a99000a81ef8f677c0d
magicers <magicers123@gmail.com>
2025-06-10 16:04:53 +0900

    shell Skeleton Code

---
+++ TestConsole/src/test/java/TestConsoleRead.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestConsoleRead {
+
+    @Test
+    void 읽기_1_LBA_PASS() {
+        String input = "read 3";
+
+    }
+}
