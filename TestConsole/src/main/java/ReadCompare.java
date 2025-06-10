41f4790a591c2fab15088877a10d07aa53c02bf4
ingyu.suh <sig45@naver.com>
2025-06-10 16:47:58 +0900

    [feature] ReadCompare 구현

--- TestConsole/src/main/java/ReadCompare.java
+++ TestConsole/src/main/java/ReadCompare.java
@@ -1,5 +1,10 @@
 public class ReadCompare{
-    public boolean execute(String commandStr) {
-        throw new RuntimeException("아직 구현 안됐어요");
+    public boolean execute(int LBA, String value) {
+        TestConsole testConsole = new TestConsole();
+
+        String commandStr="";
+        String result = testConsole.read(LBA);
+
+        return result.equals(value);
     }
 }
