e22590f0041105481e8f29be801dd8172c6824f9
ingyu.suh <sig45@naver.com>
2025-06-11 13:52:40 +0900

    [refactor] 리팩토링 + testConsole -> ConsoleService로 전환

--- TestConsole/src/main/java/Script1.java
+++ TestConsole/src/main/java/Script1.java
@@ -1,26 +1,33 @@
 public class Script1 implements Command {
-    private TestConsole testConsole;
-
-    public Script1(TestConsole testConsole) {
-        this.testConsole = testConsole;
+    public static final String TEST_VALUE = "0xFFFFFFFF";
+    public static final int LAST_LBA = 100;
+    public static final int DIV_NUM = 5;
+    private ConsoleService consoleService;
+    public Script1(ConsoleService consoleService) {
+        this.consoleService = consoleService;
     }
 
     @Override
     public void execute(String commandStr) {
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 5; j++) {
-                testConsole.write((i * 5 + j), "0xFFFFFFFF");
+        int currentLBA = 0;
+        while(currentLBA < LAST_LBA) {
+            for(int LBA = currentLBA; LBA< currentLBA + DIV_NUM;LBA++){
+                consoleService.write((LBA), TEST_VALUE);
             }
-            for (int j = 0; j < 5; j++) {
-                boolean result = testConsole.readCompare((i * 5 + j), "0xFFFFFFFF");
 
-                if (!result) {
-                    System.out.printf("FAIL");
+            for(int LBA = currentLBA; LBA < currentLBA + DIV_NUM;LBA++){
+                if (!consoleService.readCompare((LBA), TEST_VALUE)) {
+                    System.out.println("FAIL");
                     return;
                 }
             }
+
+            currentLBA += DIV_NUM;
         }
-        System.out.printf("PASS");
+        
+        
+        System.out.println("PASS");
     }
+
 }
 
