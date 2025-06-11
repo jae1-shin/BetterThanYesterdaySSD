961051f459de69f9f29885ff5da998af207840c1
ingyu.suh <sig45@naver.com>
2025-06-11 13:53:54 +0900

    [test]  testConsole -> ConsoleService로 전환에 따른 test 수정

--- TestConsole/src/test/java/ScriptsTest.java
+++ TestConsole/src/test/java/ScriptsTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,7 +12,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class ScriptsTest {
     @Mock
-    private TestConsole testConsole;
+    private ConsoleService consoleService ;
 
     @Test
     void script3_write_readCompare_실행횟수_정상() {
@@ -45,15 +46,15 @@ class ScriptsTest {
 
     @Test
     void script1_write_readCompare_실행횟수_정상() {
-        Script1 script1 = new Script1(testConsole);
+        Script1 script1 = new Script1(consoleService);
         String commandStr = "";
 
-        doReturn(true).when(testConsole).readCompare(anyInt(), anyString());
+        doReturn(true).when(consoleService).readCompare(anyInt(), anyString());
 
         script1.execute(commandStr);
 
-        verify(testConsole, times(100)).write(intThat(i -> i >= 0 && i < 100), anyString());
-        verify(testConsole, times(100)).readCompare(intThat(i -> i >= 0 && i < 100), anyString());
+        verify(consoleService, times(100)).write(intThat(i -> i >= 0 && i < 100), anyString());
+        verify(consoleService, times(100)).readCompare(intThat(i -> i >= 0 && i < 100), anyString());
     }
 
 }
