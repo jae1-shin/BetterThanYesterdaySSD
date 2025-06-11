448c2940a6e2b0e925cfadbdf01c1b2bc96b3248
dudalss-moon <test@test.com>
2025-06-11 10:01:34 +0900

    [refactor] main 함수에서 파라미터 검증 리팩토링 (상수화)

--- SsdDriver/src/main/java/Ssd.java
+++ SsdDriver/src/main/java/Ssd.java
@@ -3,18 +3,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Ssd {
+
+    public static final String OUTPUT_FILE_PATH = "ssd_output.txt";
+    public static final String DATA_FORMAT = "^0x[0-9A-Fa-f]{8}$";
+    public static final String WRITE_COMMAND = "W";
+    public static final String READ_COMMAND = "R";
+    public static final String ERROR = "ERROR";
+    public static final int ARGUMENT_COMMAND_INDEX = 0;
+    public static final int ARGUMENT_ADDRESS_INDEX = 1;
+    public static final int ARGUMENT_DATA_INDEX = 2;
+    public static final int LBA_MIN = 0;
+    public static final int LBA_MAX = 100;
+    public static final int ARGUMENT_MAX_COUNT = 3;
+
     public static void main(String[] args) {
         if (!isValidArgumentCount(args)) {
             writeError();
             return;
         }
 
-        if (!isValidCommand(args[0])) {
+        if (!isValidCommand(args[ARGUMENT_COMMAND_INDEX])) {
             writeError();
             return;
         }
 
-        if (!isValidAddressRange(args[1])) {
+        if (!isValidAddressRange(args[ARGUMENT_ADDRESS_INDEX])) {
             writeError();
             return;
         }
@@ -26,28 +39,28 @@ public class Ssd {
     }
 
     private static boolean isValidDataForWrite(String[] args) {
-        return "W".equals(args[0]) && args[2].matches("^0x[0-9A-Fa-f]{8}$");
+        return WRITE_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]) && args[ARGUMENT_DATA_INDEX].matches(DATA_FORMAT);
     }
 
     private static boolean isValidAddressRange(String address) {
         int LBA = Integer.parseInt(address);
-        if (LBA < 0 || LBA >= 100) {
+        if (LBA < LBA_MIN || LBA > LBA_MAX) {
             return false;
         }
         return true;
     }
 
     private static boolean isValidCommand(String command) {
-        return "W".equals(command) || "R".equals(command);
+        return WRITE_COMMAND.equals(command) || READ_COMMAND.equals(command);
     }
 
     private static boolean isValidArgumentCount(String[] args) {
-        return !(args.length < 2 || args.length > 3);
+        return !(args.length < ARGUMENT_DATA_INDEX || args.length > ARGUMENT_MAX_COUNT);
     }
 
     static private void writeError() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("ssd_output.txt"))) {
-            bw.write("ERROR");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+            bw.write(ERROR);
         } catch (IOException e) {
             // ignore
         }
