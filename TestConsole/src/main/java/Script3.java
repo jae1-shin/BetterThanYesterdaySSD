ea1799ec9d7d363bb4be834c88e8db276b80370f
jae1-shin <blessed9589@gmail.com>
2025-06-11 10:07:44 +0900

    [feature] scirpt3 기본 구현 / write, readCompare refactoring 뒤 반영 필요

---
+++ TestConsole/src/main/java/Script3.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Script3 implements Command{
+    private TestConsole testConsole;
+    private ReadCompare readCompare;
+
+    public Script3(TestConsole testConsole, ReadCompare readCompare) {
+        this.testConsole = testConsole;
+        this.readCompare = readCompare;
+    }
+
+    @Override
+    public void execute(String commandStr) {
+        for (int i = 0; i < 200; i++) {
+            String randomHexFor0 = getRandomHexString();
+            String randomHexFor99 = getRandomHexString();
+
+            testConsole.write("0 " + randomHexFor0);
+            testConsole.write("99 " + randomHexFor99);
+
+            if (!readCompare.execute("0 " + randomHexFor0)) {
+                System.out.println("FAIL");
+                return;
+            }
+
+            if (!readCompare.execute("99 " + randomHexFor99)) {
+                System.out.println("FAIL");
+                return;
+            }
+        }
+
+        System.out.println("PASS");
+    }
+
+    private String getRandomHexString() {
+        Random random = new Random();
+        long randomUnsignedInt = random.nextLong() & 0xFFFFFFFFL;
+
+        return "0x" + String.format("%08X", randomUnsignedInt);
+    }
+}
+
