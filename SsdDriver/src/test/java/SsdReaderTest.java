b6cf7b837c0c9cfda67fd22ce7d7fafe8c7f8bf3
hyehye <hye1.jeong@samsung.com>
2025-06-11 10:44:58 +0900

    [refactor] 파라미터 실패 검증은 main함수에서 진행

--- SsdDriver/src/test/java/SsdReaderTest.java
+++ SsdDriver/src/test/java/SsdReaderTest.java
@@ -40,22 +40,6 @@ class SsdReaderTest {
         }
     }
 
-    @Test
-    void 파라미터_0_99_사이일때_정상작동하고_출력파일에_10글자_기록됨() throws IOException {
-        reader.read(0);
-
-        String output = Files.readString(Paths.get(OUTPUT_FILE_PATH));
-        assertThat(output).isEqualTo(SAMPLE_DATA.substring(0, 10));
-    }
-
-    @Test
-    void 파라미터_0_99_아닌경우_ERROR_기록() throws IOException {
-        reader.read(100);
-
-        String output = Files.readString(Paths.get(OUTPUT_FILE_PATH));
-        assertThat(output).contains("ERROR");
-    }
-
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2})
     void 파라미터에_해당하는_데이터_읽기_성공(int LBA) throws IOException {
