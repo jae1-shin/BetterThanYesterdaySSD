d3d8a69fba64280bbf246b1e083e50fa31423b1a
jae1-shin <blessed9589@gmail.com>
2025-06-11 09:16:13 +0900

    [feature] script1 readCompare 추가

--- TestConsole/src/test/java/Script1Test.java
+++ TestConsole/src/test/java/Script1Test.java
@@ -12,14 +12,20 @@ class Script1Test {
     @Mock
     private TestConsole testConsole;
 
+    @Mock
+    private ReadCompare readCompare;
+
     @Test
-    void script1_write_실행_정상() {
-        Script1 script1 = new Script1(testConsole);
+    void script1_write_readCompare_실행횟수_정상() {
+        Script1 script1 = new Script1(testConsole, readCompare);
         String commandStr = "";
+
+        doReturn(true).when(readCompare).execute(any());
 
         script1.execute(commandStr);
 
         verify(testConsole, times(100)).write(any());
+        verify(readCompare, times(100)).execute(any());
     }
 
 }
