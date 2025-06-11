b0281c34e9488ee09d7f6b8797aa6e2caf851274
ingyu.suh <sig45@naver.com>
2025-06-11 10:24:55 +0900

    [feature] Sciprt2 내용 추가

--- TestConsole/src/main/java/Script2.java
+++ TestConsole/src/main/java/Script2.java
@@ -1,2 +1,24 @@
 public class Script2 {
+    void execute(){
+        TestConsole testConsole = new TestConsole();
+        ReadCompare readCompare = new ReadCompare();
+        int lba[] = {4,0,3,1,2};
+        String value = "0x12345678";
+
+        for(int i=0;i<30;i++){
+            for(int j=0;j<5;j++){
+                testConsole.write(lba[j], value);
+            }
+
+            for(int j=0;j<5;j++){
+                if(!readCompare.execute(j, value)){
+                    System.out.println("FAIL");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("PASS");
+    }
+
 }
