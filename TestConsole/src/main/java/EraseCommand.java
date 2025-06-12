37d28722118ad1cf9bc5f9f4d82ce91cad9f6320
interjw <interjiwoo@gmail.com>
2025-06-12 11:13:55 +0900

    first commit
    # Conflicts:
    #	TestConsole/src/main/java/WriteCommand.java

--- TestConsole/src/main/java/EraseCommand.java
+++ TestConsole/src/main/java/EraseCommand.java
@@ -16,24 +16,5 @@ public class EraseCommand extends Command{
         } catch (IndexOutOfBoundsException e) {
             System.out.println("ERROR IndexOutOfBoundsException" + e.getMessage());
         }
-
-        return ;
-    }
-
-    private static boolean InvalidCheck(String[] args) {
-
-        if (args.length != 2) {
-            System.out.println("ERROR Invalid argument numbers. ");
-            System.out.println("Usage: erase <LBA> <SIZE> or erase_range <Start LBA> <End LBA>");
-            return true;
-        }
-
-        int lba = Integer.parseInt(args[1]);
-        if (lba < 0 || lba > 99) {
-            System.out.println("ERROR LBA must be between 0 and 99.");
-            return true;
-        }
-
-        return false;
     }
 }
