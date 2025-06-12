8c17fee15ae9d6d984b6dd1cfe228dc22cb283fe
hyehye <hye1.jeong@samsung.com>
2025-06-12 15:19:50 +0900

    [refactor] BufferReader에서 BufferUtil 활용하도록 변경

--- SsdDriver/src/main/java/BufferReader.java
+++ SsdDriver/src/main/java/BufferReader.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,7 +11,7 @@ public class BufferReader {
         File[] files = bufferDir.listFiles();
         if (files == null) return "";
 
-        List<Command> commandList = getCommandList(files);
+        List<Command> commandList = BufferUtil.getCommandList(files);
 
         // 최신 명령 부터 확인
         commandList.sort(Comparator.comparingInt((Command c) -> c.order).reversed());
@@ -26,35 +25,6 @@ public class BufferReader {
         return "";
     }
 
-    private static List<Command> getCommandList(File[] files) {
-        List<Command> commandList = new ArrayList<>();
-
-        for (File file : files) {
-            String name = file.getName();
-            if (name.contains("empty")) continue;
-
-            String[] parts = name.split("_");
-
-            try {
-                int order = Integer.parseInt(parts[0]);
-                String cmdType = parts[1];
-                int lba = Integer.parseInt(parts[2]);
-
-                if (cmdType.equals("W")) {
-                    String data = parts[3];
-                    commandList.add(new Command(order, Command.Type.WRITE, lba, 1, data));
-                } else if (cmdType.equals("E")) {
-                    int size = Integer.parseInt(parts[3]);
-                    commandList.add(new Command(order, Command.Type.ERASE, lba, size, null));
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-
-        }
-        return commandList;
-    }
-
     private boolean isTargetLBAErased(int targetLBA, Command cmd) {
         return cmd.type == Command.Type.ERASE &&
                 targetLBA >= cmd.lba && targetLBA < (cmd.lba + cmd.size);
@@ -62,24 +32,6 @@ public class BufferReader {
 
     private boolean isTargetLBAWrited(int targetLBA, Command cmd) {
         return cmd.type == Command.Type.WRITE && cmd.lba == targetLBA;
-    }
-
-    static class Command {
-        enum Type { WRITE, ERASE }
-
-        int order;
-        Type type;
-        int lba;
-        int size;
-        String data;
-
-        Command(int order, Type type, int lba, int size, String data) {
-            this.order = order;
-            this.type = type;
-            this.lba = lba;
-            this.size = size;
-            this.data = data;
-        }
     }
 
 }
