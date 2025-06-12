e18a63d1099744d80e3dacd3ff81308e26f8ec06
jae1-shin <blessed9589@gmail.com>
2025-06-12 17:46:57 +0900

    [refactor] Ssd.initFiles() 완료에 따른 기존 기능 중복 삭제

--- SsdDriver/src/main/java/SsdConstants.java
+++ SsdDriver/src/main/java/SsdConstants.java
@@ -6,9 +6,9 @@ public final class SsdConstants {
 
     public static final String SSD_NAND_FILE = "ssd_nand.txt";
     public static final String OUTPUT_FILE_PATH = "ssd_output.txt";
+    public static final String BUFFER_PATH = "buffer";
     public static final int BLOCK_SIZE = 10;
     public static final int BUFFER_SIZE = 5;
-    public static final String BUFFER_PATH = "buffer";
     public static final String DEFAULT_DATA = "0x00000000";
     public static final String ERROR = "ERROR";
 
--- SsdDriver/src/main/java/SsdWriter.java
+++ SsdDriver/src/main/java/SsdWriter.java
@@ -1,21 +1,9 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class SsdWriter {
 
-    public static final String DEFAULT_DATA = "0x00000000";
-    public static final int MAX_DATA_COUNT = 100;
-
     public void write(int address, String data) throws IOException {
-        checkFileAndWriteDefaultData();
         writeData(address, data);
-    }
-
-    private void checkFileAndWriteDefaultData() throws IOException {
-        File file = new File(SsdConstants.SSD_NAND_FILE);
-        if (file.exists()) return;
-        writeDefaultData();
     }
 
     private void writeData(long address, String data) throws IOException {
@@ -24,9 +12,5 @@ public class SsdWriter {
         raf.seek(address * SsdConstants.BLOCK_SIZE);
         raf.writeBytes(data);
         raf.close();
-    }
-
-    private void writeDefaultData() throws IOException {
-        Files.writeString(Paths.get(SsdConstants.SSD_NAND_FILE), DEFAULT_DATA.repeat(MAX_DATA_COUNT));
     }
 }
