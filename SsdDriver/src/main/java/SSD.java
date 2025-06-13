cb73b1ae2f831b7d07e7b9d27879db2ad14440c6
hyehye <hye1.jeong@samsung.com>
2025-06-13 17:22:53 +0900

    [refactor] validator 클래스 분리

--- SsdDriver/src/main/java/SSD.java
+++ SsdDriver/src/main/java/SSD.java
@@ -1,5 +1,6 @@
 import command.CommandContext;
 import command.CommandFactory;
+import command.validator.ArgsValidator;
 import common.SSDConstants;
 import common.util.BufferUtil;
 
@@ -11,18 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SSD {
-    public static final String DATA_FORMAT = "^0x[0-9A-Fa-f]{8}$";
-    public static final String WRITE_COMMAND = "W";
-    public static final String READ_COMMAND = "R";
-    public static final String ERASE_COMMAND = "E";
-    public static final String FLUSH_COMMAND = "F";
-    public static final int ARGUMENT_COMMAND_INDEX = 0;
-    public static final int ARGUMENT_ADDRESS_INDEX = 1;
-    public static final int ARGUMENT_DATA_INDEX = 2;
-    public static final int LBA_MIN = 0;
-    public static final int LBA_MAX = 99;
     public static final int LBA_MAX_COUNT = 100;
-    public static final int ARGUMENT_MAX_COUNT = 3;
 
     public void processCommand(String[] args) {
         try  {
@@ -31,7 +21,7 @@ public class SSD {
             // ignore
         }
 
-        if (!checkPreCondition(args)) {
+        if (!ArgsValidator.checkPreCondition(args)) {
             writeError();
             return;
         }
@@ -67,63 +57,6 @@ public class SSD {
     private void checkAndCreateBuffer() throws IOException {
         File bufferDir = BufferUtil.checkAndCreateBufferDir();
         BufferUtil.checkAndCreateEmptyBufferFiles(bufferDir);
-    }
-
-    private boolean isWriteCommand(String[] args) {
-        return WRITE_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
-    }
-
-    private boolean isReadCommand(String[] args) {
-        return READ_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
-    }
-
-    private boolean isEraseCommand(String[] args) {
-        return ERASE_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
-    }
-
-    private boolean isFlushCommand(String[] args) {
-        return FLUSH_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
-    }
-
-    private boolean checkPreCondition(String[] args) {
-        if (!isValidArgumentCount(args)) return false;
-        if (!isValidCommand(args[ARGUMENT_COMMAND_INDEX])) return false;
-        if (!isFlushCommand(args) && !isValidAddressRange(args[ARGUMENT_ADDRESS_INDEX])) return false;
-        if (!isValidDataForWrite(args)) return false;
-        if (!isValidDataForErase(args)) return false;
-
-        return true;
-    }
-
-    private boolean isValidDataForWrite(String[] args) {
-        if (isReadCommand(args) || isEraseCommand(args) || isFlushCommand(args)) return true;
-        return isWriteCommand(args) && args[ARGUMENT_DATA_INDEX].matches(DATA_FORMAT);
-    }
-
-    private boolean isValidDataForErase(String[] args) {
-        if (isReadCommand(args) || isWriteCommand(args) || isFlushCommand(args)) return true;
-        if (Integer.parseInt(args[ARGUMENT_DATA_INDEX]) < 0 || Integer.parseInt(args[ARGUMENT_DATA_INDEX]) > 10) return false;
-        int lastLBA = Integer.parseInt(args[ARGUMENT_ADDRESS_INDEX]) + Integer.parseInt(args[ARGUMENT_DATA_INDEX]) - 1;
-        return isEraseCommand(args) && isValidAddressRange(Integer.toString(lastLBA));
-    }
-
-    private boolean isValidAddressRange(String address) {
-        int LBA = Integer.parseInt(address);
-        if (LBA < LBA_MIN || LBA > LBA_MAX) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isValidCommand(String command) {
-        return WRITE_COMMAND.equals(command) || READ_COMMAND.equals(command) || ERASE_COMMAND.equals(command) || FLUSH_COMMAND.equals(command);
-    }
-
-    private boolean isValidArgumentCount(String[] args) {
-        if (isFlushCommand(args)) {
-            return args.length == 1; // Flush command should have only one argument
-        }
-        return !(args.length < ARGUMENT_DATA_INDEX || args.length > ARGUMENT_MAX_COUNT);
     }
 
     private void writeError() {
