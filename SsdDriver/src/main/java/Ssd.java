765f87eb367749dacf75ee0dc335221c8404e617
jae1-shin <blessed9589@gmail.com>
2025-06-13 16:17:04 +0900

    [fix] F flush precondition 미비로 인한 fail 긴급 수정

--- SsdDriver/src/main/java/Ssd.java
+++ SsdDriver/src/main/java/Ssd.java
@@ -10,6 +10,7 @@ public class Ssd {
     public static final String WRITE_COMMAND = "W";
     public static final String READ_COMMAND = "R";
     public static final String ERASE_COMMAND = "E";
+    public static final String FLUSH_COMMAND = "F";
     public static final int ARGUMENT_COMMAND_INDEX = 0;
     public static final int ARGUMENT_ADDRESS_INDEX = 1;
     public static final int ARGUMENT_DATA_INDEX = 2;
@@ -75,10 +76,15 @@ public class Ssd {
         return ERASE_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
     }
 
+    private boolean isFlushCommand(String[] args) {
+        return FLUSH_COMMAND.equals(args[ARGUMENT_COMMAND_INDEX]);
+    }
+
+
     private boolean checkPreCondition(String[] args) {
         if (!isValidArgumentCount(args)) return false;
         if (!isValidCommand(args[ARGUMENT_COMMAND_INDEX])) return false;
-        if (!isValidAddressRange(args[ARGUMENT_ADDRESS_INDEX])) return false;
+        if (!isFlushCommand(args) && !isValidAddressRange(args[ARGUMENT_ADDRESS_INDEX])) return false;
         if (!isValidDataForWrite(args)) return false;
         if (!isValidDataForErase(args)) return false;
 
@@ -86,12 +92,12 @@ public class Ssd {
     }
 
     private boolean isValidDataForWrite(String[] args) {
-        if (isReadCommand(args) || isEraseCommand(args)) return true;
+        if (isReadCommand(args) || isEraseCommand(args) || isFlushCommand(args)) return true;
         return isWriteCommand(args) && args[ARGUMENT_DATA_INDEX].matches(DATA_FORMAT);
     }
 
     private boolean isValidDataForErase(String[] args) {
-        if (isReadCommand(args) || isWriteCommand(args)) return true;
+        if (isReadCommand(args) || isWriteCommand(args) || isFlushCommand(args)) return true;
         if (Integer.parseInt(args[ARGUMENT_DATA_INDEX]) < 0 || Integer.parseInt(args[ARGUMENT_DATA_INDEX]) > 10) return false;
         int lastLBA = Integer.parseInt(args[ARGUMENT_ADDRESS_INDEX]) + Integer.parseInt(args[ARGUMENT_DATA_INDEX]) - 1;
         return isEraseCommand(args) && isValidAddressRange(Integer.toString(lastLBA));
@@ -106,10 +112,14 @@ public class Ssd {
     }
 
     private boolean isValidCommand(String command) {
-        return WRITE_COMMAND.equals(command) || READ_COMMAND.equals(command) || ERASE_COMMAND.equals(command);
+        return WRITE_COMMAND.equals(command) || READ_COMMAND.equals(command) || ERASE_COMMAND.equals(command) || FLUSH_COMMAND.equals(command);
     }
 
     private boolean isValidArgumentCount(String[] args) {
+        if (isFlushCommand(args)) {
+            return args.length == 1; // Flush command should have only one argument
+        }
+
         return !(args.length < ARGUMENT_DATA_INDEX || args.length > ARGUMENT_MAX_COUNT);
     }
 
