ed166bcf071ee9f626524f286ba70aec6aef14a1
ingyu.suh <sig45@naver.com>
2025-06-13 17:13:44 +0900

    [refactor] 공통부분 추출

--- SsdDriver/src/test/java/BufferOptimizerTest.java
+++ SsdDriver/src/test/java/BufferOptimizerTest.java
@@ -15,31 +15,16 @@ import static org.assertj.core.api.Assertions.*;
 
 class BufferOptimizerTest {
 
-    void deleteBufferFolder() {
-        Path folder = Paths.get("buffer");
-        try (Stream<Path> walk = Files.walk(folder)) {
-            walk.sorted(Comparator.reverseOrder())
-                    .forEach(path -> {
-                        try {
-                            Files.delete(path);
-                        } catch (IOException e) {
-                            // ignore
-                        }
-                    });
-        } catch (IOException e) {
-            // ignore
-        }
-    }
+    Path folder = Paths.get("buffer");
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         deleteBufferFolder();
+        Files.createDirectories(folder);
     }
 
     @Test
     void BufferFull시_Flush_후_명령_추가() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
         Files.createFile(folder.resolve("3_W_22_0x12341234"));
@@ -55,8 +40,6 @@ class BufferOptimizerTest {
 
     @Test
     void IgnoreWrite_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -70,8 +53,6 @@ class BufferOptimizerTest {
 
     @Test
     void IgnoreWrite_중복_없음() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -86,8 +67,6 @@ class BufferOptimizerTest {
 
     @Test
     void IgnoreErase_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_E_18_3"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -100,8 +79,6 @@ class BufferOptimizerTest {
 
     @Test
     void IgnoreErase_중복_없음() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_E_10_3"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -116,8 +93,6 @@ class BufferOptimizerTest {
 
     @Test
     void MergeWrite_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_E_10_4"));
 
@@ -131,8 +106,6 @@ class BufferOptimizerTest {
 
     @Test
     void IgnoreErase_후_MergeWrite_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_5_0x1234ABCD"));
         Files.createFile(folder.resolve("2_W_7_0xABCDABCD"));
         Files.createFile(folder.resolve("3_E_10_2"));
@@ -145,5 +118,55 @@ class BufferOptimizerTest {
         assertThat(bufferOptimizer.getBuffer().get(0).getCommandFullName()).isEqualTo("W_5_0x1234ABCD");
         assertThat(bufferOptimizer.getBuffer().get(1).getCommandFullName()).isEqualTo("W_7_0xABCDABCD");
         assertThat(bufferOptimizer.getBuffer().get(2).getCommandFullName()).isEqualTo("E_10_5");
+    }
+
+//    @Test
+//    void Flush후_MergeErase후_IgnoreWrite() throws IOException {
+//        Files.createFile(folder.resolve("1_E_10_1"));
+//        Files.createFile(folder.resolve("2_E_11_1"));
+//        Files.createFile(folder.resolve("3_E_12_1"));
+//        Files.createFile(folder.resolve("4_E_13_1"));
+//        Files.createFile(folder.resolve("5_E_14_1"));
+//
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_15_1"));
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_2"));
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_16_1"));
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_3"));
+//        bufferController.processCommand(new Command(0, CommandType.WRITE, 15, 1, null, "W_14_0x12345678"));
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_3"));
+//
+//        assertThat(bufferController.getBuffer().size()).isEqualTo(1);
+//        assertThat(bufferController.getBuffer().get(0).getCommandFullName()).isEqualTo("E_14_3");
+//    }
+//
+//    @Test
+//    void IgnoreWrite후_IgnoreErase() throws IOException {
+//        Files.createFile(folder.resolve("1_W_10_0x12345678"));
+//
+//        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_10_2"));
+//        bufferController.processCommand(new Command(0, CommandType.WRITE, 15, 1, null, "W_11_0x12345678"));
+//        bufferController.processCommand(new Command(0, CommandType.WRITE, 15, 1, null, "W_10_0x12345678"));
+//
+//        assertThat(bufferController.getBuffer().size()).isEqualTo(3);
+//
+//        assertThat(bufferController.getBuffer().get(0).getCommandFullName()).isEqualTo("W_11_0x12345678");
+//        assertThat(bufferController.getBuffer().get(0).getCommandFullName()).isEqualTo("W_10_0x12345678");
+//
+//    }
+
+    void deleteBufferFolder() {
+        Path folder = Paths.get("buffer");
+        try (Stream<Path> walk = Files.walk(folder)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .forEach(path -> {
+                        try {
+                            Files.delete(path);
+                        } catch (IOException e) {
+                            // ignore
+                        }
+                    });
+        } catch (IOException e) {
+            // ignore
+        }
     }
 }
