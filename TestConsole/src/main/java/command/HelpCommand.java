4b07cf2e5dcca781b4b1818de5a4c8cd48592720
ingyu.suh <sig45@naver.com>
2025-06-13 11:08:22 +0900

    [feature] help 마이너한 수정

--- TestConsole/src/main/java/command/HelpCommand.java
+++ TestConsole/src/main/java/command/HelpCommand.java
@@ -15,8 +15,24 @@ public class HelpCommand extends Command {
 
     @Override
     public CommandResult doExecute(String[] args) {
-        //
-        // service.help(); 헬프가없네요! 누락된거같은데 수정부탁드립니다!
+
+        logger.result("Team: BetterThanYesterday");
+        logger.result("Members: 신재원, 정혜원, 문영민, 조효민, 류지우, 서인규");
+        logger.result("");
+        logger.result("");
+        logger.result("Available commands:");
+        logger.result("write {lba} {data} - Write data to the specified LBA");
+        logger.result("read {lba} - Read data from the specified LBA");
+        logger.result("exit - Exit the console");
+        logger.result("erase {lba} {size}- erase data from the specified LBA to size");
+        logger.result("erase_range {start_lba} {end_lba}- erase data between the specified LBA");
+        logger.result("help - Display this help message");
+        logger.result("fullwrite {data} - Write the same data to all LBAs");
+        logger.result("fullread - Read all LBAs and display their values");
+        logger.result("");
+        logger.result("Note: ");
+        logger.result("{lba} must be an integer between 0 and 99");
+        logger.result("{data} must be 4-byte unsigned hexadecimal (0x00000000 - 0xFFFFFFFF)");
         return CommandResult.PASS;
     }
 
