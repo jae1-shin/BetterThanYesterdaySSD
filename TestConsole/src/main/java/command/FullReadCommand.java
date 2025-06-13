40264b8d7e2f1f074328d37657f27139b65d6e6f
ingyu.suh <sig45@naver.com>
2025-06-13 09:43:25 +0900

    [feature] fullread/write 구조 변경

--- TestConsole/src/main/java/command/FullReadCommand.java
+++ TestConsole/src/main/java/command/FullReadCommand.java
@@ -4,14 +4,18 @@ public class FullReadCommand extends Command {
     public FullReadCommand(ConsoleService service) {
         super(service);
     }
-
+    
     @Override
-    public boolean execute(String[] args) {
+    public boolean isValid(String[] args) {
         if(!isValidArgumentNumber(args)){
             logger.error("ERROR Invalid argument numbers. Usage: read <address>");
             return false;
         }
+        return true;
+    }
 
+    @Override
+    public boolean doExecute(String[] args) {
         service.fullRead();
         return true;
     }
@@ -19,4 +23,5 @@ public class FullReadCommand extends Command {
     private static boolean isValidArgumentNumber(String[] args) {
         return args.length == 1;
     }
+
 }
