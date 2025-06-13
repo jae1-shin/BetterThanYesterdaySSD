7135bc64fab5eedae29fe9d03676f7ecf683b0f0
ingyu.suh <sig45@naver.com>
2025-06-13 16:18:07 +0900

    [refactor] 중복제거

--- SsdDriver/src/test/java/BufferControllerTest.java
+++ SsdDriver/src/test/java/BufferControllerTest.java
@@ -11,32 +11,16 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.*;
 
 class BufferControllerTest {
-
-    void deleteBufferFolder() {
-        Path folder = Paths.get("buffer");
-        try (Stream<Path> walk = Files.walk(folder)) {
-            walk.sorted(Comparator.reverseOrder())
-                    .forEach(path -> {
-                        try {
-                            Files.delete(path);
-                        } catch (IOException e) {
-                            // ignore
-                        }
-                    });
-        } catch (IOException e) {
-            // ignore
-        }
-    }
+    Path folder = Paths.get("buffer");
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         deleteBufferFolder();
+        Files.createDirectories(folder);
     }
 
     @Test
     void BufferFull시_Flush_후_명령_추가() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
         Files.createFile(folder.resolve("3_W_22_0x12341234"));
@@ -52,8 +36,6 @@ class BufferControllerTest {
 
     @Test
     void IgnoreWrite_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -67,8 +49,6 @@ class BufferControllerTest {
 
     @Test
     void IgnoreWrite_중복_없음() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_W_20_0xABCDABCD"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -83,8 +63,6 @@ class BufferControllerTest {
 
     @Test
     void IgnoreErase_중복_제거_정상() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_E_18_3"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -97,8 +75,6 @@ class BufferControllerTest {
 
     @Test
     void IgnoreErase_중복_없음() throws IOException {
-        Path folder = Paths.get("buffer");
-        Files.createDirectories(folder);
         Files.createFile(folder.resolve("1_E_10_3"));
         Files.createFile(folder.resolve("2_W_21_0x12341234"));
 
@@ -145,11 +121,10 @@ class BufferControllerTest {
     }
 
     @Test
-    void 복합버퍼알고리즘_테스트() throws IOException {
-// Given: buffer 디렉토리 초기화
+    void Flush후_MergeErase후_IgnoreWrite() throws IOException {
         Path folder = Paths.get("buffer");
         Files.createDirectories(folder);
-        // 연속 Erase → 병합 대상
+
         Files.createFile(folder.resolve("1_E_10_1"));  // LBA 10~14
         Files.createFile(folder.resolve("2_E_11_1"));  // LBA 15~19
         Files.createFile(folder.resolve("3_E_12_1"));
@@ -163,10 +138,25 @@ class BufferControllerTest {
         bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_16_1"));
         bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_3"));
         bufferController.processCommand(new Command(0, CommandType.WRITE, 15, 1, null, "W_14_0x12345678"));
-        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_2"));
+        bufferController.processCommand(new Command(0, CommandType.ERASE, 15, 1, null, "E_14_3"));
 
+        assertThat(bufferController.getBuffer().size()).isEqualTo(1);
+        assertThat(bufferController.getBuffer().get(0).getCommandFullName()).isEqualTo("E_14_3");
+    }
 
-
-
+    void deleteBufferFolder() {
+        Path folder = Paths.get("buffer");
+        try (Stream<Path> walk = Files.walk(folder)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .forEach(path -> {
+                        try {
+                            Files.delete(path);
+                        } catch (IOException e) {
+                            // ignore
+                        }
+                    });
+        } catch (IOException e) {
+            // ignore
+        }
     }
 }
