8785fad3bfb01ed63b0f658cb864c097244974ab
ingyu.suh <sig45@naver.com>
2025-06-13 16:52:57 +0900

    [fix] isValidScriptNames 수정

--- TestConsole/src/main/java/command/script/ScriptRunner.java
+++ TestConsole/src/main/java/command/script/ScriptRunner.java
@@ -56,11 +56,11 @@ public class ScriptRunner implements RunMode {
         if (scriptNames.isEmpty()) return false;
         for (String script : scriptNames) {
             if (invoker.hasCommand(script)) {
-                return false;
+                return true;
             }
         }
 
-        return true;
+        return false;
     }
 
     private boolean fileExists(String fileName) {
