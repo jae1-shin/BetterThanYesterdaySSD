7fb1116dc094fd929e060abd4bb391901b307167
hyehye <hye1.jeong@samsung.com>
2025-06-13 19:26:00 +0900

    [refactor] args count 검증은 각자 validator에서 하도록 수정

--- SsdDriver/src/main/java/command/validation/ArgsValidator.java
+++ SsdDriver/src/main/java/command/validation/ArgsValidator.java
@@ -3,15 +3,7 @@ package command.validation;
 public class ArgsValidator {
     public static final int LBA_MIN = 0;
     public static final int LBA_MAX = 99;
-
     public static final int ARGUMENT_ADDRESS_INDEX = 1;
-    public static final int ARGUMENT_DATA_INDEX = 2;
-    public static final int ARGUMENT_MAX_COUNT = 3;
-
-
-    public static boolean isValidArgumentCount(String[] args) {
-        return !(args.length < ARGUMENT_DATA_INDEX || args.length > ARGUMENT_MAX_COUNT);
-    }
 
     public static boolean isValidAddressRange(String address) {
         try {
--- SsdDriver/src/main/java/command/validation/impl/ReadCommandValidator.java
+++ SsdDriver/src/main/java/command/validation/impl/ReadCommandValidator.java
@@ -9,7 +9,7 @@ public class ReadCommandValidator implements CommandValidator {
     @Override
     public boolean validate(String[] args) {
         return READ_COMMAND.equals(args[0]) &&
-                ArgsValidator.isValidArgumentCount(args) &&
+                args.length == 2 &&
                 ArgsValidator.isValidAddressRange(args[ArgsValidator.ARGUMENT_ADDRESS_INDEX]);
     }
 }
